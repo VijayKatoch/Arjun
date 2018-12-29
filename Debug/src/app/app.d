6e9b275dd182586698073a5aa229537b61ac6efd
Vijay Katoch <katoch.vijay@gmail.com>
2018-12-30 00:41:04 +0100

    Revert "Appweb Webserver as lib added"
    This reverts commit bea777a26611ce473d5016e5d5dad69a0c841509.

--- Debug/src/app/app.d
+++
@@ -1,3 +0,0 @@
-src/app/app.o: ../src/app/app.c ../src/include/motor_control.h
-
-../src/include/motor_control.h:
--- Debug/src/hal/raspberryPi/bsp/bsp.d
+++
@@ -1,9 +0,0 @@
-src/hal/raspberryPi/bsp/bsp.o: ../src/hal/raspberryPi/bsp/bsp.c \
- ../src/hal/raspberryPi/include/pwm.h \
- ../src/hal/raspberryPi/include/gpio.h ../src/hal/raspberryPi/bsp/bsp.h
-
-../src/hal/raspberryPi/include/pwm.h:
-
-../src/hal/raspberryPi/include/gpio.h:
-
-../src/hal/raspberryPi/bsp/bsp.h:
