723a5f0145c64307709f7501f9c237e8e5d4810e
Vijay Katoch <katoch.vijay@gmail.com>
2018-12-31 18:28:41 +0100

    removed unnecessary files

--- Software/PiTank/Debug/src/hal/raspberryPi/drivers/pwm/pwm.d
+++
@@ -1,5 +0,0 @@
-src/hal/raspberryPi/drivers/pwm/pwm.o: \
- ../src/hal/raspberryPi/drivers/pwm/pwm.c \
- ../src/hal/raspberryPi/drivers/pwm/pwm.h
-
-../src/hal/raspberryPi/drivers/pwm/pwm.h:
--- Software/PiTank/Debug/src/hal/raspberryPi/motor_control_hw.d
+++
@@ -1,7 +0,0 @@
-src/hal/raspberryPi/motor_control_hw.o: \
- ../src/hal/raspberryPi/motor_control_hw.c ../src/include/motor_control.h \
- ../src/hal/raspberryPi/include/bsp.h
-
-../src/include/motor_control.h:
-
-../src/hal/raspberryPi/include/bsp.h:
